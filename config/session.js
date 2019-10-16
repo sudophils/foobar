cde698c7c2e887b8d5166517cae2a36b47b97c30
Nge Philips Ucheolisah <philips@Nges-MacBook-Pro.local>
2019-10-16 23:52:01 +0100

    added redis connect

--- config/session.js
+++ config/session.js
@@ -10,30 +10,33 @@
  */
 
 module.exports.session = {
+  adapter: "connect-redis",
+  host: "barb.redistogo.com",
+  port: 10079,
+  user: "redistogo",
+  pass: "a37c566e68ce67e3e1b61d29658ee98e",
 
   /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: 'd225da84dae8e14ef16ea281249c437c',
-
+   *                                                                          *
+   * Session secret is automatically generated when your new app is created   *
+   * Replace at your own risk in production-- you will invalidate the cookies *
+   * of your users, forcing them to log in again.                             *
+   *                                                                          *
+   ***************************************************************************/
+  secret: "d225da84dae8e14ef16ea281249c437c"
 
   /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Customize when built-in session support will be skipped.                 *
+   *                                                                          *
+   * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+   * session management when responding to simple requests for static assets, *
+   * like images or stylesheets.)                                             *
+   *                                                                          *
+   * https://sailsjs.com/config/session                                       *
+   *                                                                          *
+   ***************************************************************************/
   // isSessionDisabled: function (req){
   //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
   // },
-
 };

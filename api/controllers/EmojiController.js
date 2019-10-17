6e000835aaae4c8e57c70c1281f941294a4109d6
Nge Philips Ucheolisah <philips@Nges-MacBook-Pro.local>
2019-10-17 11:31:03 +0100

    controllers added

--- api/controllers/EmojiController.js
+++ api/controllers/EmojiController.js
@@ -6,7 +6,22 @@
  */
 
 module.exports = {
-  
-
+  associateWithUser: function(req, res) {
+    //update user and associate him with emoji
+    // res.json({ ok: "hello world" });
+    Emoji.update({ id: req.param("id") }, { owner: req.param("owner") }).exec(
+      function(err) {
+        if (err) {
+          return res.status(400).json("No user found");
+        }
+        return res
+          .status(200)
+          .json({
+            ok: `You have successfully made ${req.param(
+              "owner"
+            )} the owner of the imoji ${req.param("id")}`
+          });
+      }
+    );
+  }
 };
-
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,7 +6,35 @@
  */
 
 module.exports = {
-  
+  // GET imoji/user/:id
+  //localhost:1337/user/erwr2342342
 
+  // find a single user
+  findOne: function(req, res) {
+    User.findOne({ screenName: req.param("screenName") }).exec(function(
+      err,
+      user
+    ) {
+      if (err) {
+        throw new Error(err);
+      }
+
+      // subsribe changes to this current user
+      // will send socket event when new change occurs
+      //   User.subscribe(req, user.id);
+
+      if (!user) {
+        res.status(404).json({ err: "no user found" });
+      }
+
+      // find all the imogis that belongs to this user
+      Emoji.find({ user: user.id }).exec(function(err, emojis) {
+        if (err) {
+          throw new Error("failed tho find your emojis");
+        }
+
+        return res.view("pages/profile", { user: user, emojis: emojis });
+      });
+    });
+  }
 };
-

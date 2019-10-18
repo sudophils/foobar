12b9cbf9eb2491841f278c72d5cbfedcfb49507b
Philips Nge Ucheolisah <philencripted@gmail.com>
2019-10-18 13:11:55 +0000

    changed callback url

--- config/twitter.js
+++ config/twitter.js
@@ -1,6 +1,6 @@
 module.exports.twitter = {
   consumerKey: "g0iU3phT6pVLFthrU66YYfdhQ",
   consumerSecret: "7u2XCyixIkXYTjkw8WCjyOxd39T1YwQsqqVHpUGRSI6Ni83UZp",
-  callbackUrl: "https://emophils.herokuapp.com/"
+  callbackUrl: "https://emophils.herokuapp.com/twitter"
 };
 //https://emophils.herokuapp.com
